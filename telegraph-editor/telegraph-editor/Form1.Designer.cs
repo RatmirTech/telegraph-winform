d7588fec0625f869a013fff879711452fc26b488
Ratmir Ishgulov <ishgulov.ratmir@gmail.com>
2022-07-15 17:33:04 +0300

    add-automatic-create-chapter
    Add system : AUTO CREATE CHAPTER -> In the future ACC
    
    User can choose files from he's disk. All images was uploaded in telegra.ph
    
    User can write Title and Author in two textbox
    
    Also, after click, all data go to telegra.ph to createPage (telegra.ph/api - docs).
    
    Program rerurns to us link to the chapter

--- telegraph-editor/telegraph-editor/Form1.Designer.cs
+++ telegraph-editor/telegraph-editor/Form1.Designer.cs
@@ -33,11 +33,16 @@
             this.theDialog = new System.Windows.Forms.OpenFileDialog();
             this.listView1 = new System.Windows.Forms.ListView();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Location = new System.Drawing.Point(350, 190);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(250, 63);
             this.button1.TabIndex = 0;
@@ -51,28 +56,78 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(397, 26);
+            this.listView1.Location = new System.Drawing.Point(350, 30);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(546, 139);
+            this.listView1.Size = new System.Drawing.Size(250, 139);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 222);
+            this.button2.Location = new System.Drawing.Point(15, 154);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(250, 71);
+            this.button2.Size = new System.Drawing.Size(201, 45);
             this.button2.TabIndex = 3;
             this.button2.Text = "Создать главу";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.createChapterBtn_click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 27);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Название Главы";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 27);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Автор";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(204, 27);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 247);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(189, 38);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "{\"tag\":\"p\",\"children\":[{\"tag\":\"a\",\"attrs\":{\"href\":\"https:\\/\\/t.me\\/Ontary\",\"targe" +
+    "t\":\"_blank\"},\"children\":[\"For Sent\"]}]}";
+            this.textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 303);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(342, 140);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "{\"tag\":\"figure\",\"children\":[{\"tag\":\"img\",\"attrs\":{\"src\":\"tech\"}},{\"tag\":\"figcapti" +
+    "on\",\"children\":[\"\"]}]}";
+            this.textBox5.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(955, 599);
+            this.ClientSize = new System.Drawing.Size(729, 471);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
@@ -80,6 +135,7 @@
             this.Name = "Form1";
             this.Text = "TelegraphEditor - github RatmirTech";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -89,5 +145,10 @@
         private OpenFileDialog theDialog;
         private ListView listView1;
         private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
     }
 }
--- telegraph-editor/telegraph-editor/Form1.cs
+++ telegraph-editor/telegraph-editor/Form1.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System.Net.Http.Headers;
 
 namespace telegraph_editor
@@ -14,6 +15,10 @@ namespace telegraph_editor
         string[] filePaths;
         List<string> liv = new List<string>();
         string fileSrc;
+        string contentPage;
+        List<string> imgItems = new List<string>();
+
+
 
         private void uploadfilesBtn_click(object sender, EventArgs e)
         {
@@ -21,7 +26,7 @@ namespace telegraph_editor
 
             Stream myStream;
             theDialog.Title = "Загрузка файлов в телеграф";
-            theDialog.Filter = "JPG Files (*.jpg)| *.jpg";
+            theDialog.Filter = "JPG Files (*.jpg)| *.jpeg";
             theDialog.InitialDirectory = @"C:\Users\ratni\Desktop";
             theDialog.Multiselect = true;
             if (theDialog.ShowDialog() == DialogResult.OK)
@@ -30,39 +35,38 @@ namespace telegraph_editor
                 {
                     if ((myStream = theDialog.OpenFile()) != null)
                     {
-                        using (myStream)
+                        foreach (var item in theDialog.FileNames)
                         {
-                            foreach (var item in theDialog.FileNames)
-                            {
-                                liv.Add(item);
-                            }
-                            filePaths = liv.ToArray();
-
-                            using (var multipartFormContent = new MultipartFormDataContent())
-                            {
-                                foreach (var filePath in filePaths)
-                                {
-                                    var fileName = Path.GetFileName(filePath);
-
-                                    //Load the file and set the file's Content-Type header
-                                    var fileStreamContent = new StreamContent(File.OpenRead(filePath));
-                                    fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
-
-                                    //Add the file
-                                    multipartFormContent.Add(fileStreamContent, name: "files", fileName: fileName);
-                                    //Send it
-                                    var response = client.PostAsync("https://telegra.ph/upload/", multipartFormContent);
-                                    var resk = response.Result.Content.ReadAsStringAsync().Result.ToString();
-
-                                    char[] ipt = { '{', '}', '[', ']', '\\', ':', '"' };
-                                    string imagePathTelegraph = resk.Trim(ipt);
-                                    imagePathTelegraph = imagePathTelegraph.Remove(0, 14);
-                                    fileSrc = "https://telegra.ph/file/" + imagePathTelegraph;
-                                    listView1.Items.Add(fileSrc);
-                                    liv.Add(fileSrc);
-                                }
-                            }
+                            liv.Add(item);
                         }
+                        filePaths = liv.ToArray();
+
+                        foreach (var filePath in filePaths)
+                        {
+                            var multipartFormContent = new MultipartFormDataContent();
+                            var fileName = Path.GetFileName(filePath);
+
+                            //Load the file and set the file's Content-Type header
+                            var fileStreamContent = new StreamContent(File.OpenRead(filePath));
+                            fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+
+                            //Add the file
+                            multipartFormContent.Add(fileStreamContent, name: "files", fileName: fileName);
+                            //Send it
+                            var response = client.PostAsync("https://telegra.ph/upload/", multipartFormContent);
+                            var resk = response.Result.Content.ReadAsStringAsync().Result.ToString();
+
+                            char[] ipt = { '{', '}', '[', ']', '\\', ':', '"' };
+                            string imagePathTelegraph = resk.Trim(ipt);
+                            imagePathTelegraph = imagePathTelegraph.Remove(0, 14);
+                            fileSrc = "https://telegra.ph/file/" + imagePathTelegraph;
+                            listView1.Items.Add(fileSrc);
+                            imgItems.Add(fileSrc);
+
+                            fileStreamContent = null;
+                            multipartFormContent = null;
+                        }
+
                     }
                 }
                 catch (Exception ex)
@@ -74,8 +78,37 @@ namespace telegraph_editor
 
         private void createChapterBtn_click(object sender, EventArgs e)
         {
-            var content = new StringContent("");
-            var response = client.PostAsync("", content);
+
+            string authorUrl = textBox4.Text;
+            string imageContent = "";
+
+            foreach (var item in imgItems)
+            {
+                imageContent = imageContent + textBox5.Text.Replace("tech", item.ToString()) + ',';
+            }
+            int x1 = imageContent.Length - 1;
+            imageContent = imageContent.Remove(x1);
+            contentPage = "[" + authorUrl + "," + imageContent + "]";
+
+
+            string url = "https://api.telegra.ph/createPage";
+            var parameters = new Dictionary<string, string>
+            {
+                { "access_token", "d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722" },
+                { "title", $"{textBox1.Text}" },
+                { "author_name", $"{textBox2.Text}" },
+                { "content", $"{contentPage}" }
+            };
+            var encodedContent = new FormUrlEncodedContent(parameters);
+
+            var response = client.PostAsync(url, encodedContent);
+            var result = response.Result.Content.ReadAsStringAsync().Result;
+
+            dynamic jsonResponse = JsonConvert.DeserializeObject(result);
+            string urlString = jsonResponse.result.url;
+
+            textBox3.Text = urlString;
         }
+        //{"ok":true,"result":{"path":"MyChapter-07-14","url":"https:\/\/telegra.ph\/MyChapter-07-14","title":"MyChapter","description":"","author_name":"Ontary","views":0,"can_edit":true}}
     }
 }
